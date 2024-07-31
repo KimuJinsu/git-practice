b7f4d632357fce42d42411fe45ab821ed91e9d52
Jinsu <jinsu8828@gmail.com>
2024-07-31 22:38:13 +0900

    jinsu3 add

--- src/GitTest/Main.java
+++ src/GitTest/Main.java
@@ -6,5 +6,7 @@ public class Main {
         System.out.println("Jinsu");
         System.out.println("Jinsu");
         System.out.println("Jinsu");
+        System.out.println("Jinsu");
+        System.out.println("Jinsu");
     }
 }
